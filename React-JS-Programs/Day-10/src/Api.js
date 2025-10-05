d8c66312f58e28e8960109ec0b1c07fdb80ce1ec
Shreyas Deshpande <deshpandeshreyas434@gmail.com>
2025-10-05 21:36:39 +0530

    Deleted root Day-01 to Day-08 and React-JS-Programs Day-09 to Day-11

--- React-JS-Programs/Day-10/src/Api.js
+++
@@ -1,9 +0,0 @@
-// src/Api.js
-import axios from "axios";
-
-// For testing, we can use JSONPlaceholder fake API
-const API = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com",
-});
-
-export const getAllUsers = () => API.get("/users"); // Returns 10 users
--- React-JS-Programs/Day-10/src/components/Applayout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white text-center py-4 mt-6">
-      <p>© {new Date().getFullYear()} MyApp. All Rights Reserved.</p>
-    </footer>
-  );
-}
